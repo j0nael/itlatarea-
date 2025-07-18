4f5b897142688f0a4a34141415fc884fb69b46ef
Jonathan Geraldo <Thucientifico10@gmail.com>
2025-07-18 12:32:32 -0400

    code completion

---
+++ task#4/app contac orientado a ojecto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace app_contac_orientado_a_ojecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
